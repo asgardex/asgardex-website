2970e128d61a85b3f9f3f8df10149063f9079720
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2025-01-02 12:47:46 +0930

    udpate release page notes

--- src/app/installer/page.tsx
+++ src/app/installer/page.tsx
@@ -62,13 +62,13 @@ export default async function InstallerPage () {
           Visit our github for detailed release notes and signed binaries
         </h3>
         <div className='text-m md:text-lg font-normal mb-8 text-dark-gray'>
-          <h2>v1.22.9 Release notes</h2>
+          <h2>v1.30.0 Release notes</h2>
           <div className='flex text-sm justify-center'>
             <ul className='list-disc list-inside text-left text-sm md:text-base'>
-              <li>Fixed errors with BCH client</li>
-              <li>Fixed errors with Dash client</li>
+              <li>Fixed Ledger UTXO chain errors</li>
+              <li>Major UI updates across the app</li>
               <li>See GH for full release notes</li>
-              <li>Mac still experiencing issues when opening even though we have a valid dev cert, enter settings - privacy & security - scroll down - open anyway</li>
+              <li>Mac still experiencing issues when opening, enter settings - privacy & security - scroll down - open anyway</li>
               <li>A work around for this issue is to download the asset directly from Github</li>
             </ul>
           </div>
