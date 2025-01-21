b9d72586409c4620a3bba3daaf6f08cbf8ff52bf
cinnamoroll6130 <nextmission4260@gmail.com>
2025-01-21 12:30:45 +0000

    feat: update first section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from 'next'
+import Image from 'next/image'
 import { Providers } from './providers'
 import './globals.css'
 import { jakarta } from './ui/fonts'
 import Header from './ui/Header'
 import Footer from './ui/Footer'
+
 export const metadata: Metadata = {
   title: 'Asgardex',
   description: 'Template generated for Kairos'
 }
 
-export default function RootLayout ({
+export default function RootLayout({
   children
 }: {
   children: React.ReactNode
@@ -17,6 +19,14 @@ export default function RootLayout ({
   return (
     <html lang="en">
       <body className={`${jakarta.className} antialiased`}>
+        <Image
+          className="w-full h-screen fixed -z-10 opacity-20"
+          src="/background.avif"
+          alt=""
+          sizes="100vw"
+          width={100}
+          height={100}
+        />
         <Providers>
           <Header />
           {children}
