1a8dee65fd92d657bb7c9dea9003ac4c6aaf0204
cinnamoroll6130 <nextmission4260@gmail.com>
2025-01-21 12:30:33 +0000

    chore: update header component

--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -1,25 +1,46 @@
 'use client'
-import Image from 'next/image'
-import { play } from './fonts'
-import Link from 'next/link'
-import { IconMenu2, IconX } from '@tabler/icons-react'
 import { useState } from 'react'
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, NavbarMenuToggle, NavbarMenu } from '@nextui-org/react'
-import ThemeSwitcher from '../themeSwitcher'
-import { useTheme } from 'next-themes' // Use the correct import for next-themes
 
-export default function Header () {
+import clsx from 'clsx'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useTheme } from 'next-themes' // Use the correct import for next-themes
+import { IconMenu2, IconX } from '@tabler/icons-react'
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+  NavbarMenuToggle,
+  NavbarMenu
+} from '@nextui-org/react'
+
+import { play } from './fonts'
+
+export default function Header() {
   const [isMenuOpen, setMenuOpen] = useState(false)
   const { theme } = useTheme() // Get the theme from next-themes
 
   return (
-    <Navbar shouldHideOnScroll onMenuOpenChange={setMenuOpen}>
+    <Navbar maxWidth="xl" onMenuOpenChange={setMenuOpen}>
       <NavbarContent>
         <NavbarBrand>
-          <Link href={'/'}>
-          <div className={'flex items-center justify-between'}>
-              <Image src={'/asgardex-logo.avif'} alt={'Asgardex logotipe'} width={40} height={40} />
-              <h1 className={`${play.className} font-bold text-asgardex-dark-1000 text-2xl uppercase ml-3 ${theme === 'dark' ? 'invert' : ''}`}>Asgardex</h1>
+          <Link href="/">
+            <div className="flex items-center justify-between">
+              <Image
+                src={'/asgardex-logo.avif'}
+                alt="Asgardex logotipe"
+                width={40}
+                height={40}
+              />
+              <h1
+                className={clsx(
+                  play.className,
+                  'font-bold invert text-asgardex-dark-1000 text-2xl uppercase ml-3'
+                )}>
+                Asgardex
+              </h1>
             </div>
           </Link>
         </NavbarBrand>
@@ -27,71 +48,18 @@ export default function Header () {
 
       <NavbarContent className="hidden md:flex" justify="end">
         <NavbarItem>
-          <Link href={'https://twitter.com/asgardex'} target="_blank">
-            <Image
-              src={'/x-black-logo.avif'}
-              alt={'Asgardex X'}
-              width={24}
-              height={24}
-              className={theme === 'dark' ? 'invert' : ''}
-            />
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href={'https://discord.gg/AZDtabWFJF'} target="_blank">
-            <Image
-              src={'/discord-black-logo.avif'}
-              alt={'Asgardex Discord'}
-              width={24}
-              height={24}
-              className={theme === 'dark' ? 'invert' : ''}
-            />
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href={'https://github.com/asgardex/asgardex-desktop/releases'} target="_blank">
-            <Image
-              src={'/github-black-logo.avif'}
-              alt={'Asgardex Discord'}
-              width={24}
-              height={24}
-              className={theme === 'dark' ? 'invert' : ''}
-            />
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href={'https://thorchain.org/'} target="_blank">
-            <Image
-              src={'/thorchain-black-logo.avif'}
-              alt={'Thorchain logotipe'}
-              width={24}
-              height={24}
-              className={theme === 'dark' ? 'invert' : ''}
-            />
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href={'https://www.mayaprotocol.com/'} target="_blank">
-            <Image
-              src={'/maya-black-logo.avif'}
-              alt={'Maya Protocol logotipe'}
-              width={24}
-              height={24}
-              className={theme === 'dark' ? 'invert' : ''}
-            />
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
           <Link href={'/installer'}>
-            <Button className="bg-asgardex-primary-500 px-11 rounded-lg text-lg text-asgardex-dark-1000 h-11">Download</Button>
+            <Button className="bg-asgardex-primary-500 px-8 rounded-full text-lg text-asgardex-dark-1000 h-10">
+              Download
+            </Button>
           </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <ThemeSwitcher />
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarMenuToggle className="sm:hidden text-asgardex-dark-1000" icon={isMenuOpen ? <IconX /> : <IconMenu2 />} />
+      <NavbarMenuToggle
+        className="sm:hidden text-asgardex-dark-1000"
+        icon={isMenuOpen ? <IconX /> : <IconMenu2 />}
+      />
 
       <NavbarMenu className="h-screen bg-white items-center">
         <div className="p-4 flex w-full items-center justify-around">
@@ -113,7 +81,9 @@ export default function Header () {
               className={theme === 'dark' ? 'invert' : ''}
             />
           </Link>
-          <Link href={'https://github.com/asgardex/asgardex-desktop/releases'} target="_blank">
+          <Link
+            href={'https://github.com/asgardex/asgardex-desktop/releases'}
+            target="_blank">
             <Image
               src={'/github-black-logo.avif'}
               alt={'Asgardex Discord'}
@@ -143,7 +113,9 @@ export default function Header () {
         </div>
 
         <Link href={'/installer'}>
-          <Button className="bg-asgardex-primary-500 px-11 rounded-lg text-lg text-asgardex-dark-1000">Download</Button>
+          <Button className="bg-asgardex-primary-500 px-11 rounded-lg text-lg text-asgardex-dark-1000">
+            Download
+          </Button>
         </Link>
       </NavbarMenu>
     </Navbar>
